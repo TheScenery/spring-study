3d4468984520150920680f34d5d0f569e2fffcf2
Wenbin Liu <liuwenbin.lwb@gmail.com>
2023-07-28 23:08:57 +0800

    hello spring

---
+++ spring-02-hello-spring/src/main/java/io/thescenery/App.java
@@ -0,0 +1,17 @@
+package io.thescenery;
+
+import io.thescenery.pojo.Hello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Hello hello = (Hello)context.getBean("hello");
+        System.out.println(hello.getStr());
+    }
+}
---
+++ spring-02-hello-spring/src/main/java/io/thescenery/pojo/Hello.java
@@ -0,0 +1,13 @@
+package io.thescenery.pojo;
+
+public class Hello {
+    private String str;
+
+    public void setStr(String str1) {
+        str = str1;
+    }
+
+    public String getStr() {
+        return str;
+    }
+}
